e1f17a8d6c5d031dd5da464dc21fa8cfec920932
Jay de Almeida <hildajadebispo@outlook.com>
2022-03-23 23:00:32 -0300

    das

---
+++ src/Cliente.java
@@ -0,0 +1,23 @@
+
+public class Cliente {
+
+	private String nome;
+	private Number CPF;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Number getCPF() {
+		return CPF;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCPF(Number CPF) {
+		this.CPF = CPF;
+	}
+
+}
